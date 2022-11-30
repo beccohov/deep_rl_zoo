87558870da70092ee6467ed2453da7157f7d8c92
Arkadii <36795440+beccohov@users.noreply.github.com>
2022-11-30 20:47:19 +0300

    Update agent.py

--- deep_rl_zoo/rudolf/agent.py
+++ deep_rl_zoo/rudolf/agent.py
@@ -1,5 +1,7 @@
 import deep_rl_zoo.types as types_lib
 import torch
+import numpy as np
+import torch
 
 class RudolphAgent(types_lib.Agent):
   def __init__(self, model, checkpoint_path, api, args, spc_tokens):
